c7614356c7bd2dffe268268d37901d7c16ac7ed8
tomifarall <39673189+tomifarall@users.noreply.github.com>
2020-11-26 12:38:10 -0300

    Agrego funciones mcm y mcd

---
+++ mcfunctions.c
@@ -0,0 +1,10 @@
+unsigned int mcd(unsigned int m, unsigned int n){
+    if (n == 0) return m;
+    return mcd(n, m % n);
+}
+
+unsigned int mcm(unsigned int m, unsigned int n) {
+    int numerator = m * n;
+    unsigned int denominator = mcd(m, n);
+    return numerator/denominator;
+}
